53119b9e0b8e9500e3ae3d697d31fd5a3d63780a
Iago Leonardo Laguna <iagolaguna.83@gmail.com>
2016-11-28 21:12:00 -0200

    arrumando bugs :alien:

--- server/v1/routesServer.js
+++ server/v1/routesServer.js
@@ -26,9 +26,9 @@ function register(server, connection) {
         method: 'POST',
         handler: function (request, reply) {
             var password = request.payload.password;
-            var user = request.payload.email;
-            console.log('Receving new User:"' + user + '" and password :"' + password + '"');
-            var query = 'SELECT * FROM user WHERE email = "' + user + '" and password = "' + password + '"';
+            var email = request.payload.email;
+            console.log('Receving new User:"' + email + '" and password :"' + password + '"');
+            var query = 'SELECT * FROM user WHERE email = "' + email + '" and password = "' + password + '"';
             console.log(query);
 
             connection.query(query, function (err, rows) {
