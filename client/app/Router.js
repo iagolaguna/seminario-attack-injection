1cfeed7f5d40b1ce77794f1c58a3168cb6852834
Marcos Vinicius <Marcos Vinicius>
2016-11-28 23:25:38 -0200

    Finalizando o projeto

--- client/app/Router.js
+++ client/app/Router.js
@@ -6,9 +6,13 @@ function Router($routeProvider) {
     $routeProvider
         .when('/login', {
             templateUrl: './app/views/login.html',
-            controller:'AppController',
-            controllerAs:'vm'
-        })  .when('/', {
+            controller: 'AppController',
+            controllerAs: 'vm'
+        }).when('/', {
             templateUrl: './app/views/home.html',
+        }).when('/cadastrar', {
+            templateUrl: './app/views/cadastrar.html',
+            controller: 'AppController',
+            controllerAs: 'vm'
         });
 }
--- client/app/controllers/AppController.js
+++ client/app/controllers/AppController.js
@@ -1,56 +1,70 @@
 angular
     .module('app')
-    .controller('AppController',AppController);
+    .controller('AppController', AppController);
 
-    AppController.$inject = ['$http'];
+AppController.$inject = ['$http'];
 
-    function AppController($http){
-       
-       var vm = this;
+function AppController($http) {
 
-       var url = 'http://localhost:8000/v1/user/login';
+    var vm = this;
 
-       vm.logar = logar;
+    var url = 'http://localhost:8000/v1/user/login';
 
-       vm.success = false;
-       
-       vm.close =  close;
-       
-       vm.catch = false;
+    vm.logar = logar;
 
-       vm.usuario = {};
+    vm.success = false;
 
-       vm.changeUrl = changeUrl;
+    vm.dialogCadastro = false;
 
-       function changeUrl(){
-           if(vm.secure){
-               url = 'http://localhost:8000/v1/user/login/secure';
-           }else{
-               url = 'http://localhost:8000/v1/user/login';
-           }
-       }
+    vm.close = close;
 
-       function close(){
-           vm.success=false;
-           vm.catch=false;
-           vm.showDialog = false;
-           vm.usuario = {};
-       }
+    vm.catch = false;
 
+    vm.usuario = {};
 
-       function logar(){
-           $http.post(url,vm.usuario).then(function(response){
-               vm.showDialog = true;
-               console.log(response.data);
-               if(response.data != ""){
-                   vm.success = true;
-                   vm.usuario = response.data;
-               }else{
-                 vm.catch = true;
-               }
-           }).catch(function(err){
-               console.log(err);
-           });
-       }
-    
+    vm.cadastrar = cadastrar;
+
+    vm.changeUrl = changeUrl;
+
+    function cadastrar() {
+        $http.post('http://localhost:8000/v1/user/add', vm.usuario).then(function (response) {
+            console.log('foi');
+            vm.dialogCadastro = true;
+        }).catch(function (err) {
+            console.log(err);
+        });
     }
+
+    function changeUrl() {
+        if (vm.secure) {
+            url = 'http://localhost:8000/v1/user/login/secure';
+        } else {
+            url = 'http://localhost:8000/v1/user/login';
+        }
+    }
+
+    function close() {
+        vm.success = false;
+        vm.catch = false;
+        vm.showDialog = false;
+        vm.dialogCadastro = false;
+        vm.usuario = {};
+    }
+
+
+    function logar() {
+        $http.post(url, vm.usuario).then(function (response) {
+            vm.showDialog = true;
+            console.log(response.data);
+            if (response.data != "") {
+                vm.success = true;
+                vm.usuario = response.data;
+            } else {
+                vm.catch = true;
+            }
+        }).catch(function (err) {
+            console.log(err);
+        });
+    }
+
+}
